3b993b012609bb0e28ea821da2aac0f5b0b011c9
PrinceTG80 <tgavkhare80@gmail.com>
2022-04-14 23:00:17 +0530

    Project adding done

---
+++ src/app/homepage/add-project/add-project.page.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder,FormGroup } from '@angular/forms';
+import { SharedServiceService } from 'src/app/service/shared-service.service';
+import { NavigationExtras, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-add-project',
+  templateUrl: './add-project.page.html',
+  styleUrls: ['./add-project.page.scss'],
+})
+export class AddProjectPage implements OnInit {
+
+  public requestForm: FormGroup;
+  isSubmitted = false;
+  public arrModule = [];
+  public arrTasks = [];
+  public arrObjectives = [];
+
+  constructor(public formBuilder: FormBuilder,
+    public sharedService: SharedServiceService,
+    public router: Router) { }
+
+  ngOnInit() {
+    this.requestForm = this.formBuilder.group({
+      subcategory:['Category/'+this.sharedService.postData.category+'/'+this.sharedService.postData.subcategory],
+      modules:['2'],
+      tasks:['2'],
+      objectives:['2'],
+      title :'',
+      description:'',
+      arrModule: [this.sharedService.postData,this.sharedService.postData,this.sharedService.postData,this.sharedService.postData],
+      image: File
+    });
+  }
+
+  getPostInfo(post,i) {
+    const navigationExtras: NavigationExtras = { state: {data:post , postId: this.sharedService.myPostId[i]} };
+    this.router.navigate(['homepage/my-project'], navigationExtras);
+  }
+
+  createArrModule(sizeModule){
+    this.arrModule = [];
+    for(var i=0;i<sizeModule;i++){
+        this.arrModule.push(this.arrTasks);
+    }
+  }
+
+  createArrTasks(sizeTasks){
+    this.arrTasks = [];
+    for(var i=0;i<sizeTasks;i++){
+        this.arrTasks.push(this.arrObjectives);
+    }
+  }
+
+  createArrObjectives(sizeObjectives){
+    this.arrObjectives = [];
+    for(var i=0;i<sizeObjectives;i++){
+        this.arrObjectives.push(this.sharedService.postData);
+    }
+  }
+}
---
+++ src/app/service/shared-service.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SharedServiceService {
+
+  constructor() { }
+
+  myPostCategories = ['IT','Software','Hardware','Professional','Mixed'];
+
+  postData = {
+    subcategory: 'SUBCATEGORY',
+    category: 'CATEGORY',
+    title: 'TITLE',
+    range: '10',
+    description: 'DESCRIPTION',
+    status: 'pending',
+    date: 'date',
+    expiryDate: 'expiry',
+    time: 'time',
+    attachments: {
+      image: '../assets/images/Romantic.jpg',
+      camera: '',
+      audio: '',
+      document: '',
+    },
+    minLat: 0,
+    maxLat: 0,
+    minLong: 0,
+    maxLong: 0,
+    userName: 'Tushar',
+  };
+
+  myPost = [this.postData,this.postData,this.postData,this.postData];
+
+  myPostId = [this.postData,this.postData,this.postData,this.postData];
+
+}
